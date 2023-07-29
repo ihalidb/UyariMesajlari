885746c5311e18571c8afb1fd851c4944852297c
İbrhaim Halid Bayrak <ibrahimh.bayrak@gmail.com>
2023-07-29 18:31:43 +0300

    hata mesajı oluşturuldu

--- UyariMesajlari/ViewController.swift
+++ UyariMesajlari/ViewController.swift
@@ -9,10 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var password2TextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func signUpTiklandi(_ sender: Any) {
+        
+        let uyariMesaji = UIAlertController(title: "Hata Mesajı!", message: "Email Yanlış Verildi!", preferredStyle: UIAlertController.Style.alert)
+        
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default) { UIAlertAction in
+            // Ok Butonuna tıklanınca olacaklar
+            print("Ok butonuna tıklandı!")
+        }
+        
+        uyariMesaji.addAction(okButton)
+        
+        self.present(uyariMesaji, animated: true, completion: nil)
+        
+        
+    }
+    
 
 
 }
